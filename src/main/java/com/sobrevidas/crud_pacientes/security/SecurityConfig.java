0d176d1f9187a7d6d2a6e84e28befde3193627fb
Claudio Ferreira <claudio23@discente.ufg.br>
2025-11-14 21:07:13 -0300

    feat(security): configurar respostas 401 e 403 no tratamento de exceções

--- src/main/java/com/sobrevidas/crud_pacientes/security/SecurityConfig.java
+++ src/main/java/com/sobrevidas/crud_pacientes/security/SecurityConfig.java
@@ -2,10 +2,13 @@ package com.sobrevidas.crud_pacientes.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +25,13 @@ public class SecurityConfig {
                                 .jwtAuthenticationConverter(new JWTConverter())
                         )
                 );
+
+        http
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                        .accessDeniedHandler(new AccessDeniedHandlerImpl())
+                );
+
         return http.build();
     }
 }
