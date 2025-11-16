e266a1909d1b8470c34896f7507f59e98045ff2b
Claudio Ferreira <claudio23@discente.ufg.br>
2025-11-16 19:05:13 -0300

    feat: configurar autenticação OAuth2 do Keycloak no Swagger

--- src/main/java/com/sobrevidas/crud_pacientes/config/OpenApiConfig.java
+++ src/main/java/com/sobrevidas/crud_pacientes/config/OpenApiConfig.java
@@ -3,6 +3,14 @@ package com.sobrevidas.crud_pacientes.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -20,4 +28,31 @@ import org.springframework.context.annotation.Configuration;
         }
 )
 public class OpenApiConfig {
+
+    @Value("${keycloak.swagger.auth-url}")
+    private String authorizationUrl;
+
+    @Value("${keycloak.swagger.token-url}")
+    private String tokenUrl;
+
+    private final String SECURITY_SCHEME_NAME = "keycloak-oauth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .type(SecurityScheme.Type.OAUTH2)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .flows(new OAuthFlows()
+                                        .authorizationCode(new OAuthFlow()
+                                                .authorizationUrl(authorizationUrl)
+                                                .tokenUrl(tokenUrl)
+                                        )
+                                )
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
+    }
 }
